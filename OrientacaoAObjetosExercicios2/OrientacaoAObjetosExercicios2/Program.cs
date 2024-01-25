08bfa519c69ed25c4889f20e8763ff71e257f125
Diego Santana <3c.santana@gmail.com>
2024-01-25 13:27:13 -0300

    Resoluçaõ do exercicio 2

---
+++ OrientacaoAObjetosExercicios2/OrientacaoAObjetosExercicios2/Program.cs
@@ -0,0 +1,21 @@
+﻿using OrientacaoAObjetosExercicios2;
+using System.Globalization;
+
+Retangulo retangulo = new Retangulo();
+
+Console.WriteLine("Entre com a largura e altura do retângulo: ");
+Console.Write("Largura: ");
+double largura  = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Altura: ");
+double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+retangulo.Largura = largura;
+retangulo.Altura = altura;
+
+double area = retangulo.Area();
+double perimetro = retangulo.Perimetro();
+double diagonal = retangulo.Diagonal();
+
+Console.WriteLine($"AREA = {area.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"PERÍMETRO = {perimetro.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"DIAGONAL = {diagonal.ToString("F2", CultureInfo.InvariantCulture)}");
