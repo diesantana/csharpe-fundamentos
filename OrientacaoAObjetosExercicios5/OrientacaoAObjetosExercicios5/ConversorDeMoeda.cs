da8470add63ebb1c7201c10bf5ce3363cf58817f
Diego Santana <3c.santana@gmail.com>
2024-01-25 15:16:33 -0300

    Exercicio de fixação

---
+++ OrientacaoAObjetosExercicios5/OrientacaoAObjetosExercicios5/ConversorDeMoeda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoAObjetosExercicios5
+{
+    internal class ConversorDeMoeda
+    {
+        /// <summary>
+        ///     Recebe a cotação do dolar e o valor desejado da compra
+        /// </summary>
+        /// <param name="cotacao">Double que representa a cotação do dolar</param>
+        /// <param name="valorCompra">Double que representa o valor a ser comprado em Reais</param>
+        /// <returns> retorna o valor a ser pago com o imposto de 6%</returns>
+        public static double ValorPagarReais(double cotacao, double valorCompra)
+        {
+            double valorEmDolar = valorCompra * cotacao;
+            return valorEmDolar + (valorEmDolar * 0.06);
+        }
+    }
+}
---
+++ OrientacaoAObjetosExercicios5/OrientacaoAObjetosExercicios5/Program.cs
@@ -0,0 +1,15 @@
+﻿using OrientacaoAObjetosExercicios5;
+using System.Globalization;
+
+Console.WriteLine("EXERCÍCIO: CONVERSOR DE MOEDAS");
+Console.WriteLine();
+
+Console.Write("Qual é a cotação do dólar? ");
+double cotacaoDolar  = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dólares você vai comprar? ");
+double dolaresQuantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double dolarEmReal = ConversorDeMoeda.ValorPagarReais(cotacaoDolar, dolaresQuantidade);
+
+Console.WriteLine($"Valor a ser pago em reais = {dolarEmReal.ToString("F2", CultureInfo.InvariantCulture)}");
