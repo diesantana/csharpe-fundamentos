59badf4c9cb99ca16efe0565cb30a844bc6b6114
Diego Santana <3c.santana@gmail.com>
2024-01-24 22:35:26 -0300

    Aula sobre for

--- EstruturaCondicional/EstruturaCondicional/Program.cs
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -6,18 +6,19 @@ namespace EstruturaCondicional
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite um número: ");
-            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+            int soma = 0;
 
-            while (numero > 0)
+            for (int i = 0; i < quantidade; i++)
             {
-                double raiz = Math.Sqrt(numero);
-                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
-
-                Console.Write("Digite outro número: ");
-                numero = int.Parse(Console.ReadLine());
+                Console.Write($"Valor #{i+1}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
             }
-            Console.WriteLine("Número negativo!");
+
+            Console.WriteLine($"Soma = {soma}");
+
         }
     }
 }
