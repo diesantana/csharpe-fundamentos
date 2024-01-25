3a17ec97b2752f1d7c6dc31a97ba58f56f572ffe
Diego Santana <3c.santana@gmail.com>
2024-01-24 22:01:22 -0300

    Exercicio sobre entrada de dados

---
+++ ExercicioEntradaDeDados/ExercicioEntradaDeDados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa:");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double precoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] dados = Console.ReadLine().Split(' ');
+
+            string sobrenome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(precoProduto.ToString("F2", NumberFormatInfo.InvariantInfo));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
