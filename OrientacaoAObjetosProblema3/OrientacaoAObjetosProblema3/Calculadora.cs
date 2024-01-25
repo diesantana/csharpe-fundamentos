0c065e7b8d12609b3da20d342321c67a25ff7c95
Diego Santana <3c.santana@gmail.com>
2024-01-25 14:45:10 -0300

    Solução 2: Classe com membros de instância

---
+++ OrientacaoAObjetosProblema3/OrientacaoAObjetosProblema3/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoAObjetosProblema3
+{
+    internal class Calculadora
+    {
+        public double Raio;
+        public double PI = 3.14;
+
+        public double Circunferencia()
+        {
+            return 2.0 * PI * Raio;
+        }
+
+        public double Volume()
+        {
+            return 4.0 / 3.0 * PI * Math.Pow(Raio, 3);
+        }
+
+    }
+}
--- OrientacaoAObjetosProblema3/OrientacaoAObjetosProblema3/Program.cs
+++ OrientacaoAObjetosProblema3/OrientacaoAObjetosProblema3/Program.cs
@@ -1,29 +1,17 @@
 ﻿
 
+using OrientacaoAObjetosProblema3;
 using System.Globalization;
 
-internal class Program
-{
-    static double PI = 3.14;
-    private static void Main(string[] args)
-    {
-        Console.Write("Entre o valor do raio: ");
-        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        double circunferencia = Circunferencia(raio);
-        double volume = Volume(raio);
+Calculadora calculadora = new Calculadora();
 
-        Console.WriteLine($"Circunferência: {circunferencia.ToString("F2", CultureInfo.InvariantCulture )}");
-        Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-        Console.WriteLine($"Valor de PI: {PI.ToString("F2", CultureInfo.InvariantCulture)}");
-    }
+Console.Write("Entre o valor do raio: ");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+calculadora.Raio = raio;
 
-    static double Circunferencia(double raio)
-    {
-        return 2.0 * PI * raio;
-    }
+double circunferencia = calculadora.Circunferencia();
+double volume = calculadora.Volume();
 
-    static double Volume(double raio)
-    {
-        return 4.0 / 3.0 * PI * Math.Pow(raio, 3);
-    }
-}
+Console.WriteLine($"Circunferência: {circunferencia.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Valor de PI: {calculadora.PI.ToString("F2", CultureInfo.InvariantCulture)}");
