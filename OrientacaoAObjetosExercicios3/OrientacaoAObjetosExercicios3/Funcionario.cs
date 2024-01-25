267f49fab6c546a4c05b5abad9fbd9d5eea39634
Diego Santana <3c.santana@gmail.com>
2024-01-25 13:27:25 -0300

    Resoluçaõ do exercicio 3

---
+++ OrientacaoAObjetosExercicios3/OrientacaoAObjetosExercicios3/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoAObjetosExercicios3
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruo;
+        public double Imposto;
+
+        public double SalarioLiquido() 
+        { 
+            return SalarioBruo - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double porcentagemAumento = porcentagem / 100.00;
+            SalarioBruo += SalarioBruo * porcentagemAumento;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoAObjetosExercicios3/OrientacaoAObjetosExercicios3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using OrientacaoAObjetosExercicios3;
+using System.Globalization;
+
+Funcionario funcionario = new Funcionario();
+
+Console.WriteLine("Entre com os dados do funcionário:");
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Salário Bruto: ");
+double salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Imposto: ");
+double imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+funcionario.Nome = nome;
+funcionario.SalarioBruo = salarioBruto;
+funcionario.Imposto = imposto;
+Console.WriteLine();
+
+Console.WriteLine("Funcionário: " +  funcionario);
+Console.WriteLine();
+
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+double porcentagemAumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+funcionario.AumentarSalario(porcentagemAumento);
+
+Console.WriteLine("Dados atualizos: " + funcionario);
