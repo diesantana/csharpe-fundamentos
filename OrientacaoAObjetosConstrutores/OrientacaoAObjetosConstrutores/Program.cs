baff578b02ddd9d51ea54abbb73dc4ca0f211537
Diego Santana <3c.santana@gmail.com>
2024-01-25 20:31:32 -0300

    Construtores em classes

---
+++ OrientacaoAObjetosConstrutores/OrientacaoAObjetosConstrutores/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using OrientacaoAObjetosConstrutores;
+using System.Globalization;
+
+Console.WriteLine("Entre os dados do produto: ");
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Preço: ");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantidade no estoque: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+Produto produto = new(nome, preco, quantidade);
+
+Console.WriteLine();
+Console.WriteLine($"Dados do produto: {produto}");
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+Console.WriteLine();
+Console.WriteLine($"Dados atualizados: {produto}");
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos a ser removido do estoque: ");
+produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+Console.WriteLine();
+Console.WriteLine($"Dados atualizados: {produto}");
