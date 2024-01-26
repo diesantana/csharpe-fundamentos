52ca4b640a335c92faa0d73d86fdeb72f10c3189
Diego Santana <3c.santana@gmail.com>
2024-01-25 22:45:35 -0300

    Introdução ao encapsulamento com properties

---
+++ OrientacaoAObjetosProperties/OrientacaoAObjetosProperties/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using OrientacaoAObjetosProperties;
+
+Produto produto = new Produto("TV", 900.0, 10);
+
+Console.WriteLine(produto.Nome); // TV
+
+produto.Nome = "T"; // O VALOR NÃO É ACEITO 
+
+Console.WriteLine(produto.Nome); // TV
+
+produto.Nome = "TV 4K";
+
+Console.WriteLine(produto.Nome);
