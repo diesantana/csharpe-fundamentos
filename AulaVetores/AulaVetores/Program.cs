ab6efef3eac277bcdee81689b67cd6ced180b0f6
Diego Santana <3c.santana@gmail.com>
2024-01-26 19:49:45 -0300

    Aula sobre vetores

---
+++ AulaVetores/AulaVetores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+Console.Write("Quantas pessoas: ");
+int n = int.Parse(Console.ReadLine());
+double[] alturas = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Digite a altua da {i + 1}° pessoa: ");
+    alturas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+double media = alturas.Average();
+Console.WriteLine("Altura média = " + media.ToString("F2", CultureInfo.InvariantCulture));
