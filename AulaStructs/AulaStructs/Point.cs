7499b7db9ebcfacebecf107f7f4559cd6b8d9488
Diego Santana <3c.santana@gmail.com>
2024-01-26 18:35:53 -0300

    Aula sobre structs e comportamento de memória

---
+++ AulaStructs/AulaStructs/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaStructs
+{
+    struct Point
+    {
+        public double X, Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
+
+
---
+++ AulaStructs/AulaStructs/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AulaStructs;
+
+Point p;
+p.X = 10;
+p.Y = 20;
+Console.WriteLine(p); // (10, 20)
+
+p = new Point();
+Console.WriteLine(p); // (0, 0)
