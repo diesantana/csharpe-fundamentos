372df47b513ff1983b1e1cc902bc4b8965d103e6
Diego Santana <3c.santana@gmail.com>
2024-01-28 19:58:32 -0300

    Aula sobre modificador de parametros params

---
+++ AulaParams/AulaParams/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaParams
+{
+    internal class Calculator
+    {
+        public static double Sum(params double[] numbers)
+        {
+            double sum = 0.0;
+            foreach (double number in numbers) sum += number;
+            return sum;
+        }
+        
+    }
+}
---
+++ AulaParams/AulaParams/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using AulaParams;
+
+double result = Calculator.Sum(10, 20, 30, 50);
+Console.WriteLine(result);
