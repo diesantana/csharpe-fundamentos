74fa09e2e19b2c76687557e72627d30337762b84
Diego Santana <3c.santana@gmail.com>
2024-01-28 09:22:54 -0300

    Solução para o exercício sobre vetores (Sistema de hospedagem)

---
+++ AulaVetoresExercicio/AulaVetoresExercicio/Guest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaVetoresExercicio
+{
+    internal class Guest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Guest() { }
+        public Guest(string name, string email, int quarto)
+        {
+            Name = name;
+            this.Email = email;
+            Room = quarto;
+        }
+
+        public override string ToString()
+        {
+            return Room + ": " + Name + ", " + Email; 
+        }
+    }
+}
---
+++ AulaVetoresExercicio/AulaVetoresExercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using AulaVetoresExercicio;
+using System.Globalization;
+
+Console.Write("Quantos quartos serão alugados? ");
+int numberGuests = int.Parse(Console.ReadLine());
+Guest[] guests = new Guest[10];
+
+for (int i = 0; i < numberGuests; i++)
+{
+    Console.WriteLine($"Aluguel #{i+1}:");
+    Console.Write("Nome: ");
+    string name = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int room = int.Parse(Console.ReadLine());
+    guests[room] = new Guest(name, email, room);
+    Console.WriteLine();
+}
+
+Console.WriteLine("Quartos ocupados:");
+foreach (Guest room in guests)
+{
+    if(room != null) Console.WriteLine(room);
+}
