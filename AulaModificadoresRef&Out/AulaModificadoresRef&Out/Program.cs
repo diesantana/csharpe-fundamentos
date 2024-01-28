fc116f4716a3eb3ab6d1d3a7deb2c2e97ffd675f
Diego Santana <3c.santana@gmail.com>
2024-01-28 19:59:13 -0300

    Aula sobre os modificadores de parametro ref e out (code smalls)

---
+++ AulaModificadoresRef&Out/AulaModificadoresRef&Out/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using AulaModificadoresRef_Out;
+
+int a = 10; // valor =  10 
+Calculator.Triple(ref a);
+Console.WriteLine(a); // valor = 30
+
+int b = 10;
+int triple;
+Calculator.Triple(b, out triple);
+
+Console.WriteLine(triple); // 30
