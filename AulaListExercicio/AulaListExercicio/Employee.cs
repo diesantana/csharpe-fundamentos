81d043dd31feb3908d5ce610c76744452aa4986b
Diego Santana <3c.santana@gmail.com>
2024-01-28 22:22:30 -0300

    solucção do exercício sobre listas

---
+++ AulaListExercicio/AulaListExercicio/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaListExercicio
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee() { }
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            double increase = (percentage / 100.0) * Salary;
+            Salary += increase;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ AulaListExercicio/AulaListExercicio/Program.cs
@@ -0,0 +1,54 @@
+﻿using AulaListExercicio;
+using System.Globalization;
+
+Console.Write("Quantos funcionários serão registrados? ");
+int quantity = int.Parse(Console.ReadLine());
+
+List<Employee> employees = new List<Employee>();
+// Cadastra os funcionários
+for (int i = 0; i < quantity; i++)
+{
+    Console.WriteLine($"Funcionário #{i + 1}:");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+
+    // Verifica se o id já existe
+    while (employees.Any(x => x.Id == id))
+    {
+        Console.WriteLine("O Id informado já existe!");
+        Console.Write("Digite outro Id: ");
+        id = int.Parse(Console.ReadLine());
+    }
+
+    Console.Write("Nome: ");
+    string name = Console.ReadLine();
+    Console.Write("Salário: ");
+    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    employees.Add(new Employee(id, name, salary));
+    Console.WriteLine();
+}
+
+Console.Write("Informe o Id do funcionário que terá aumento salarial: ");
+int idToIncrease = int.Parse(Console.ReadLine());
+
+if (employees.Any(x => x.Id == idToIncrease))
+{
+    int index = employees.FindIndex(x => x.Id == idToIncrease);
+    Console.Write("Insira a porcentagem: ");
+    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    employees[index].IncreaseSalary(percentage);
+}
+else
+{
+    Console.WriteLine("Este ID não existe");
+}
+
+Console.WriteLine();
+Console.WriteLine("Lista de funcionários atualizada:");
+foreach (Employee employee in employees)
+{
+    Console.WriteLine(employee);
+}
+
+
