f367348955e7f1faa0481d35a77806d1f23c28f8
Diego Santana <3c.santana@gmail.com>
2024-01-29 21:21:53 -0300

    Aula sobre o tipo var e inferencia de tipos

---
+++ AulaVar&InferenciaDeTipos/AulaVar&InferenciaDeTipos/Program.cs
@@ -0,0 +1,11 @@
+﻿
+var a = 15;
+var b = 7.55;
+var c = "Maria";
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+// suponha que no seu programa você quer receber um inteiro na varável d
+var d = c;
+// O compilar não vai saber disso e vai receber qualquer tipo, por isso é melhor evitar o uso do var.
