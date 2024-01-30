b326f30f6e9f4519327e35e3c6d75b6948b29494
Diego Santana <3c.santana@gmail.com>
2024-01-29 22:31:48 -0300

    Aula sobre cconversão de DateTime para string

---
+++ AulaDateTimaParaString/AulaDateTimaParaString/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+DateTime agora = DateTime.Now;
+
+string s1 = agora.ToLongDateString();
+string s2 = agora.ToLongTimeString();
+string s3 = agora.ToShortDateString();
+string s4 = agora.ToShortTimeString();
+string s5 = agora.ToString();
+string s6 = agora.ToString("dd MMMM yyyy"); // 29 janeiro 2024
+
+
+Console.WriteLine(agora);
+Console.WriteLine();
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+Console.WriteLine(s4);
+Console.WriteLine(s5);  
+Console.WriteLine(s6);
