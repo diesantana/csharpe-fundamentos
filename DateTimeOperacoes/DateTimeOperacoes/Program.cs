c4bb423a3bdff2455593ea03a61e80fce4692399
Diego Santana <3c.santana@gmail.com>
2024-01-29 22:32:16 -0300

    Aula sobre operações com DateTime

---
+++ DateTimeOperacoes/DateTimeOperacoes/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+DateTime agora = DateTime.Now; // 29/01/2024 21:50:51
+DateTime d1 = agora.AddDays(7); // 05/02/2024 21:51:08
+
+TimeSpan t1 = TimeSpan.FromDays(1); // 1 dia 
+
+// subtrai 1 dia do DateTime
+DateTime d2 = agora.Subtract(t1); // 28/01/2024 22:01:26
+
+
+DateTime d3 = new DateTime(2023, 10, 15);
+DateTime d4 = DateTime.Now;
+
+TimeSpan diferenca = d4.Subtract(d3); // 106.22:05:38.7234736
+// 106 dias 22 horas 05 minutos ...
+
+
+
+
+
+Console.WriteLine(agora);
+Console.WriteLine();
+
+Console.WriteLine(d1);
+Console.WriteLine(d2);
+Console.WriteLine(diferenca);
