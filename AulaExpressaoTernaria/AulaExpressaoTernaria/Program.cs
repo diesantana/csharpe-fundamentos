f4fba4e1176960c4ae0f03cb3c803df1cf786536
Diego Santana <3c.santana@gmail.com>
2024-01-29 21:22:31 -0300

    Aula sobre expressao ternaria

---
+++ AulaExpressaoTernaria/AulaExpressaoTernaria/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Digite um valor: ");
+double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double desconto = (valor > 20.0) ? valor * 0.05 : valor * 0.1;
+Console.WriteLine("Desconto de: " + desconto);
