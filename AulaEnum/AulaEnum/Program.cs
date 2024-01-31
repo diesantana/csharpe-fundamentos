872d72798c8c1d62de4d301e20688edbb85481b8
Diego Santana <3c.santana@gmail.com>
2024-01-31 16:02:48 -0300

    Aula sobre enums

---
+++ AulaEnum/AulaEnum/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using AulaEnum.Entities;
+using AulaEnum.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine("Pedido: " + order);
+// Pedido: 1080, 31/01/2024 15:25:08, PendingPayment
+Console.WriteLine();
+
+// Convertendo um enum para string
+string txt = OrderStatus.PendingPayment.ToString();
+Console.WriteLine(txt);
+
+// String para enum
+string texto = "Delivered";
+OrderStatus orderStatus = (OrderStatus)Enum.Parse(typeof(OrderStatus), texto);
+Console.WriteLine();
+
+
+// String para enum com segurança
+string textoEnum = "Delivered";
+OrderStatus statusPedido;
+if (Enum.TryParse(textoEnum, out statusPedido))
+{
+    // A conversão foi bem-sucedida, 'statusPedido' contém o valor do enum correspondente
+    Console.WriteLine("Status do pedido: " + statusPedido);
+}
+else
+{
+    // A string fornecida não corresponde a nenhum valor do enum
+    Console.WriteLine("Status do pedido não encontrado. Por favor digite um valor válido!");
+}
