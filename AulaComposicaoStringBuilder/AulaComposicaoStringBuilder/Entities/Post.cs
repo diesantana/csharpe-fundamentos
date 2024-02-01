b889e0304873e46ffabba1bd20f6bd38e3fc229e
Diego Santana <3c.santana@gmail.com>
2024-01-31 22:46:08 -0300

    feat: Solução para o exercício resolvido, sem assistir a aula

---
+++ AulaComposicaoStringBuilder/AulaComposicaoStringBuilder/Entities/Post.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AulaComposicaoStringBuilder.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public override string ToString()
+        {
+            return Title + "\n" +
+                Likes + " Likes - " + Moment + "\n" + 
+                Content;
+        }
+    }
+}
---
+++ AulaComposicaoStringBuilder/AulaComposicaoStringBuilder/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using AulaComposicaoStringBuilder.Entities;
+
+DateTime dateTime = DateTime.Parse("21/06/2018 13:05:44");
+string title = "Traveling to New Zealand";
+string content = "I'm going to visit this wonderful country!";
+int likes = 12;
+
+Post post1 = new Post(dateTime, title, content, likes);
+string comment1 = "Have a nice trip";
+string comment2 = "Wow that's awesome!";
+
+post1.Comments.Add(new Comment(comment1));
+post1.Comments.Add(new Comment(comment2));
+
+Console.WriteLine(post1);
+Console.WriteLine("Comments:");
+foreach (Comment comment in post1.Comments)
+{
+    Console.WriteLine(comment);
+}
+
+Console.WriteLine();
+
+dateTime = DateTime.Parse("28/07/2018 23:14:19");
+title = "Good night guys";
+content = "See you tomorrow";
+likes = 5;
+
+Post post2 = new Post(dateTime, title, content, likes);
+comment1 = "Good night";
+comment2 = "May the Force be with you";
+
+post2.Comments.Add(new Comment(comment1));
+post2.Comments.Add(new Comment(comment2));
+
+Console.WriteLine(post2);
+Console.WriteLine("Comments:");
+foreach (Comment comment in post2.Comments)
+{
+    Console.WriteLine(comment);
+}
