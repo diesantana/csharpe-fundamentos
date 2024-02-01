7cf8a9221291d8beaaf356d3dee4b28913df9456
Diego Santana <3c.santana@gmail.com>
2024-02-01 14:10:22 -0300

    Aula sobre sobreposição, override e a palavra chave virtual

---
+++ AulaSobreposicao/AulaSobreposicao/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using AulaSobreposicao.Entities;
+
+Account account = new Account(1001, "Maria", 500.0);
+Account account2 = new SavingsAccount(50, "Bob", 500.0, 0.01);
+
+account.Withdraw(10.0);
+account2.Withdraw(10.0);
+
+Console.WriteLine(account.Balance); // 485.00
+Console.WriteLine(account2.Balance); // 490.00
