b8af57ef9fd7fde6be632a9e5f38f79daa192680
Diego Santana <3c.santana@gmail.com>
2024-02-01 14:09:57 -0300

    Aula sobre upcasting e downcasting + conversão segura com a palavra-chave is

---
+++ AulaUpcastingDowncasting/AulaUpcastingDowncasting/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaUpcastingDowncasting.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Deposit(balance);
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ AulaUpcastingDowncasting/AulaUpcastingDowncasting/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using AulaUpcastingDowncasting.Entities;
+
+Account acc = new Account(1001, "Alex", 0.0);
+BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+// Upcasting
+Account acc1 = bacc;
+Account acc2 = new BusinessAccount(1003, "Pedro", 0.0, 500.0);
+Account acc3 = new SavingsAccount(1004, "Alice", 0.0, 0.01);
+
+// Downcasting
+// BusinessAccount acc4 = (BusinessAccount)acc2;
+BusinessAccount acc4 = acc2 as BusinessAccount;
+
+if (acc3 is BusinessAccount)
+{
+    BusinessAccount acc5 = acc3 as BusinessAccount;
+}
+else
+{
+    Console.WriteLine("Operação insegura! Downcasting não permitido.");
+}
