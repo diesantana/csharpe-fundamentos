d09a3616091e92150e48c5617b3f23004b31aa12
Diego Santana <3c.santana@gmail.com>
2024-02-02 19:33:24 -0300

    Aula sobre try-catch

---
+++ AulaTryCatch/AulaTryCatch/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+try
+{
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+
+    int result = n1 / n2;
+    Console.WriteLine(result);
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Não é possível dividir por zero!");
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Error: " + e.Message);
+}
+
+Console.WriteLine("Continua a execução");
