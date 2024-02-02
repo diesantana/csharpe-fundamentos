2ba955ffc41c973e61328ce7c2aad100180e2588
Diego Santana <3c.santana@gmail.com>
2024-02-01 23:30:24 -0300

    Aula sobre classes abstratas

---
+++ AulaClassesAbstratas/AulaClassesAbstratas/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using AulaClassesAbstratas.Entities;
+using System.Globalization;
+using System.Security.Principal;
+
+List<Account> accounts = new List<Account>();
+
+accounts.Add(new BusinessAccount(1001, "Alex", 500.0, 200.0));
+accounts.Add(new SavingsAccount(1002, "Maria", 500.0, 0.01));
+accounts.Add(new BusinessAccount(1003, "Bob", 500.0, 200.0));
+accounts.Add(new SavingsAccount(1004, "Paulo", 500.0, 0.01));
+
+double sum = 0;
+foreach (Account account in accounts)
+{
+    sum += account.Balance;
+}
+
+Console.WriteLine("Total balance: $" + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+foreach (Account account in accounts)
+{
+    account.Withdraw(10.0);
+}
+
+Console.WriteLine("Update balance:");
+foreach (Account account in accounts)
+{
+    Console.WriteLine($"Conta: {account.Number}, Balance: $ {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+}
