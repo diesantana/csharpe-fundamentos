72a7b576a407bc8935fcc13740af617b4f097317
Diego Santana <3c.santana@gmail.com>
2024-02-02 21:49:37 -0300

    feat: criando uma classe de exceções personalizadas

---
+++ AulaTratamentoDeExcecoes/AulaTratamentoDeExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AulaTratamentoDeExcecoes.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string? message) : base(message)
+        {
+        }
+    }
+}
