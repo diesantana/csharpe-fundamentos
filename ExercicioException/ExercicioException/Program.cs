01bc3a31ea159c0b457e58eebfa9938d2540e7c6
Diego Santana <3c.santana@gmail.com>
2024-02-02 23:20:55 -0300

    feat: solução de desafio proposto sobre tratamento de exceções

---
+++ ExercicioException/ExercicioException/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using ExercicioException.Entities;
+using ExercicioException.Entities.Exceptions;
+using System.Globalization;
+
+try
+{
+    Console.WriteLine("Enter account data");
+    Console.Write("Number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+    Console.Write("Initial balance: ");
+    double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.Write("Withdraw limit: ");
+    double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Account account = new Account(number, holder, initialBalance, limit);
+    Console.WriteLine();
+
+    Console.Write("Enter amount for withdraw: ");
+    double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    account.Withdraw(amount);
+    Console.WriteLine($"New Balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+} 
+catch (DomainException e)
+{
+    Console.WriteLine("Withdraw error: " +  e.Message);
+}
