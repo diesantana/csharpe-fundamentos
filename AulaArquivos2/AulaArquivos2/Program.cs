c1eb6368986458503f92f39518a2a947f02226b9
Diego Santana <3c.santana@gmail.com>
2024-02-03 23:44:08 -0300

    feat: Instanciando um StreamReader a partir de um FileReader com o fechamento manual

---
+++ AulaArquivos2/AulaArquivos2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string path = @"c:\temp\file1.txt";
+FileStream fs = null;
+StreamReader sr = null;
+
+try
+{
+    fs = new FileStream(path, FileMode.Open); // abre o arquivo
+    sr = new StreamReader(fs);                // Stream para Ler o arquivo 
+    string linha = sr.ReadLine();
+    Console.WriteLine(linha);
+}
+catch (IOException e)
+{
+    Console.WriteLine("An Error occurred:");
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    if(sr != null) sr.Close();  //fecha o StreamReader se ele estiver aberto (!null)
+    if(fs != null) fs.Close();  //fecha o FileStream se ele estiver aberto (!null)
+}
