8fa159094d88b9c8ea4a41082d67f6d8a320a172
Diego Santana <3c.santana@gmail.com>
2024-02-04 02:52:40 -0300

    feat: Solução do exercício sobre manipulação de arquivos

---
+++ ExercicioArquivos/ExercicioArquivos/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioArquivos.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue() => Price * Quantity;
+
+        public string TagProduct () => Name + "," + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
---
+++ ExercicioArquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using ExercicioArquivos.Entities;
+using System.Globalization;
+
+string sourcePath = @"c:\temp\file.csv";
+List<Product> products = new List<Product>();
+
+try
+{
+    Directory.CreateDirectory(@"c:\temp\out");
+    string fullTargetPath = @"c:\temp\out\summary.csv";
+    string[] lines = File.ReadAllLines(sourcePath);
+    foreach (string line in lines)
+    {
+        string[] filds = line.Split(',');
+        string name = filds[0];
+        double price = double.Parse(filds[1], CultureInfo.InvariantCulture);
+        int quantity = int.Parse(filds[2]);
+
+        products.Add(new Product(name, price, quantity));
+    }
+
+    using (StreamWriter streamWriter = File.CreateText(fullTargetPath))
+    {
+        foreach (Product product in products) { streamWriter.WriteLine(product.TagProduct()); }
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error ocurred");
+    Console.WriteLine(e.Message);
+}
