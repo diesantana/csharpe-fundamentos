d9da10e59c6628e40df99849b2b54194971fffc8
Diego Santana <3c.santana@gmail.com>
2024-02-04 00:25:58 -0300

    refactor: Utilizando o using block para instanciar o FileStream e um StreamReader a partir do FileStream.

--- AulaArquivos2/AulaArquivos2/Program.cs
+++ AulaArquivos2/AulaArquivos2/Program.cs
@@ -1,24 +1,20 @@
 ﻿
 
 string path = @"c:\temp\file1.txt";
-StreamReader sr = null;
-
 try
 {
-    sr = File.OpenText(path);  // Instancia um StreamReader a partir do File, com UTF8 (File.OpenText) 
-    while (!sr.EndOfStream)
+    using (FileStream fs = new FileStream(path, FileMode.Open))
     {
-        string line = sr.ReadLine();
-        Console.WriteLine(line);
+        using (StreamReader sr = new StreamReader(fs))
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
     }
-
 }
 catch (IOException e)
 {
     Console.WriteLine("An Error occurred:");
     Console.WriteLine(e.Message);
-}
-finally
-{
-    if (sr != null) sr.Close();  //fecha o StreamReader se ele estiver aberto (!null)
 }
