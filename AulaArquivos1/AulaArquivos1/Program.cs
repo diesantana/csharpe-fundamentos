9d465a605634ef627fb7f1ce02f87bdcbd09ff98
Diego Santana <3c.santana@gmail.com>
2024-02-03 23:23:02 -0300

    Aula sobre tratamento e manipulação de arquivos: File e FileInfo

---
+++ AulaArquivos1/AulaArquivos1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string sourcePath = @"c:\temp\file1.txt";
+string targetPath = @"c:\temp\file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    //fileInfo.CopyTo(targetPath);
+    File.Copy(sourcePath, @"c:\temp\file3.txt");
+
+    string[] lines = File.ReadAllLines(sourcePath);
+    Console.WriteLine("File ReadAllLines:");
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+
+    bool exist = File.Exists(targetPath);
+    Console.WriteLine("Exist: " + exist);
+
+
+}
+
+catch(IOException e)
+{
+    Console.WriteLine("An error ocurred:");
+    Console.WriteLine(e.Message);
+}
