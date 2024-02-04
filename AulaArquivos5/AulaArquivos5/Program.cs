b51b8c341942b748fb330b9fd95585451e6f8c23
Diego Santana <3c.santana@gmail.com>
2024-02-04 02:52:11 -0300

    Aula sobre informações de Path com a classe Path

---
+++ AulaArquivos5/AulaArquivos5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string path = @"c:\temp\file1.txt";
+
+char separadorDiretorio = Path.DirectorySeparatorChar;
+Console.WriteLine("separadorDiretorio: " + separadorDiretorio);
+
+char separadorCaminho = Path.PathSeparator;
+Console.WriteLine("separadorCaminho: " + separadorCaminho);
+
+string directoryName = Path.GetDirectoryName(path);
+Console.WriteLine("Directory name: " + directoryName);
+
+
