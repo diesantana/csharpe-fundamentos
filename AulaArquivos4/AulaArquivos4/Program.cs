906828874df5d64d390d99515e63e7a81b566c71
Diego Santana <3c.santana@gmail.com>
2024-02-04 02:51:01 -0300

    Aula sobre manipulação de diretórios com as classes Directory e DirectoryInfo

---
+++ AulaArquivos4/AulaArquivos4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string path = @"C:\temp";
+
+try
+{
+    // listando as pastas 
+    var folders = Directory.GetDirectories(path);
+    foreach (var folder in folders)
+    {
+        Console.WriteLine(folder);
+    }
+
+    Console.WriteLine();
+    // listando os arquivos em uma pasta
+    var files = Directory.GetFiles(path);
+    foreach(var file in files)
+    {
+        Console.WriteLine(file);
+    }
+
+    Directory.CreateDirectory(path + @"\pasta teste\pasta dentro do teste");
+    Console.WriteLine("Criado!");
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
