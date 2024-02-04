57ac4824222583d608a9114d46bf214c195ce2b2
Diego Santana <3c.santana@gmail.com>
2024-02-04 01:03:11 -0300

    feat: Trabalhando com a classe StreamWriter

---
+++ AulaArquivos3/AulaArquivos3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string source = @"c:\temp\file1.txt";
+string targetPath = @"c:\temp\new file.txt";
+
+try
+{
+    // ler o arquivo de origem e armazenar o seu conteúdo
+    string[] lineSource = File.ReadAllLines(source);
+
+    using (StreamWriter streamWriter = File.CreateText(targetPath))
+    {
+        foreach (string line in lineSource)
+        {
+            streamWriter.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
