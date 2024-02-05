8801711d45d9704264f569da68dbdec8744a3968
Diego Santana <3c.santana@gmail.com>
2024-02-05 11:48:14 -0300

    Classes abstratas e herança

---
+++ AulaInterfaces2/AulaInterfaces2/Models/Entities/Shape.cs
@@ -0,0 +1,16 @@
+﻿using AulaInterfaces2.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaInterfaces2.Models.Entities
+{
+    internal abstract class Shape
+    {
+        public Color Color { get; set; }
+        public int MyProperty { get; set; }
+        public abstract double Area();
+    }
+}
