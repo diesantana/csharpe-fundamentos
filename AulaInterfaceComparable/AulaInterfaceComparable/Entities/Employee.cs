e70159e48e3b5b389d26668e3d5f6b27c520aaa8
Diego Santana <3c.santana@gmail.com>
2024-02-05 23:02:58 -0300

    feat: dois critérios de comparação em classes criadas, utilizando a interface Comparable

--- AulaInterfaceComparable/AulaInterfaceComparable/Entities/Employee.cs
+++ AulaInterfaceComparable/AulaInterfaceComparable/Entities/Employee.cs
@@ -1,4 +1,9 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AulaInterfaceComparable.Entities
 {
@@ -7,7 +12,7 @@ namespace AulaInterfaceComparable.Entities
         public string Name { get; set; }
         public double Salary { get; set; }
 
-        public Employee(string csvEmployee)
+        public Employee(string csvEmployee) 
         {
             string[] line = csvEmployee.Split(',');
             Name = line[0];
@@ -16,13 +21,20 @@ namespace AulaInterfaceComparable.Entities
 
         public int CompareTo(Employee? other)
         {
-            return Name.CompareTo(other.Name);
+            // dois critérios de comparação
+            int result =  Name.CompareTo(other.Name);
+            if(result == 0)
+            {
+                result = Salary.CompareTo(other.Salary);
+            }
+
+            return result;
+            // return Name.CompareTo(other.Name);
         }
 
         public override string ToString()
         {
             return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
-
         }
     }
 }
