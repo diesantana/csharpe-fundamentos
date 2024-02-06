b5f20f5694675bdd756fd0b1b8edcc031eec29f1
Diego Santana <3c.santana@gmail.com>
2024-02-06 11:58:04 -0300

    refactor: Solução utilizando generics

---
+++ AulaGenerics/AulaGenerics/Model/Services/PrintService.cs
@@ -0,0 +1,25 @@
+﻿namespace AulaGenerics.Model.Services
+{
+    internal class PrintService<T>
+    {
+        public static void AddValue(List<T> list, T value)
+        {
+            list.Add(value);
+        }
+
+        public static T First(List<T> list)
+        {
+            return list[0];
+        }
+
+        public static void Print(List<T> list)
+        {
+            Console.Write("[" + list[0]);
+            for (int i = 1; i < list.Count; i++)
+            {
+                Console.Write(", " + list[i]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ AulaGenerics/AulaGenerics/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using AulaGenerics.Model.Services;
+/*
+List<int> numbers = new List<int>();
+Console.Write("How many values (max 10)? ");
+int valuesQuantity = int.Parse(Console.ReadLine());
+if(valuesQuantity > 10)
+{
+    Console.WriteLine("Invalid quantity (max 10)");
+    while (valuesQuantity > 10)
+    {
+        Console.Write("Enter another value (max 10): ");
+        valuesQuantity = int.Parse(Console.ReadLine());
+    }
+}
+for (int i = 0; i < valuesQuantity; i++)
+{
+    numbers.Add(int.Parse(Console.ReadLine()));
+}
+
+PrintService<int>.Print(numbers);
+int first =  PrintService<int>.First(numbers);
+Console.WriteLine("First: " + first);
+*/
+List<string> nomes = new List<string>();
+Console.Write("How many values (max 10)? ");
+int valuesQuantity = int.Parse(Console.ReadLine());
+if (valuesQuantity > 10)
+{
+    Console.WriteLine("Invalid quantity (max 10)");
+    while (valuesQuantity > 10)
+    {
+        Console.Write("Enter another value (max 10): ");
+        valuesQuantity = int.Parse(Console.ReadLine());
+    }
+}
+for (int i = 0; i < valuesQuantity; i++)
+{
+    nomes.Add(Console.ReadLine());
+}
+
+PrintService<string>.Print(nomes);
+string first = PrintService<string>.First(nomes);
+Console.WriteLine("First: " + first);
