c7646234507ab8509151709af96a7d80ba73eef7
Diego Santana <3c.santana@gmail.com>
2024-02-06 12:32:23 -0300

    implementação sem o uso dos generics

---
+++ Aulagenerics2/Aulagenerics2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Aulagenerics2.Model.Services;
+
+List<int> numbers = new List<int>();
+
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    numbers.Add(x);
+}
+
+CalculationService calculationService = new CalculationService();
+int max = calculationService.Max(numbers);
+Console.WriteLine("Max: ");
+Console.WriteLine(max);
