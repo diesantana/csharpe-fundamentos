28a4a2ab4241b6e01f29017f7a5e9caab5eaf04b
Diego Santana <3c.santana@gmail.com>
2024-02-05 22:49:44 -0300

    feat: Implementando a interface Comparable em classes criadas

---
+++ AulaInterfaceComparable/AulaInterfaceComparable/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace AulaInterfaceComparable.Entities
+{
+    internal class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] line = csvEmployee.Split(',');
+            Name = line[0];
+            Salary = double.Parse(line[1], CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Employee? other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
--- AulaInterfaceComparable/AulaInterfaceComparable/Program.cs
+++ AulaInterfaceComparable/AulaInterfaceComparable/Program.cs
@@ -1,32 +1,29 @@
 ﻿
 
-string path = @"C:\temp\in.txt";
-List<string> names = new List<string>();
+using AulaInterfaceComparable.Entities;
+
+string path = @"C:\temp\in.csv";
+List<Employee> employees = new List<Employee>();
 try
 {
     using (StreamReader sr = File.OpenText(path))
     {
         while (!sr.EndOfStream)
         {
-            names.Add(sr.ReadLine());
+            employees.Add(new Employee(sr.ReadLine()));
         }
+        // Comparação na ordem natural
+        employees.Sort();
+
+        foreach (Employee emp in employees)
+        {
+            Console.WriteLine(emp);
+        }
+
     }
-    // Comparação na ordem natural
-    names.Sort();
 
-    // Lógica parsonalizada
-    // names.Sort((x, y) => x.Length.CompareTo(y.Length));
-
-    // Comparação na ordem decrescente 
-    //names.Sort();
-    //names.Reverse();
-
-    foreach (string name in names)
-    {
-        Console.WriteLine(name);
-    }
 }
-catch(IOException e)
+catch (IOException e)
 {
     Console.WriteLine("Error: ");
     Console.WriteLine(e.Message);
