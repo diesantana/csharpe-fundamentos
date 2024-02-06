2e074c3e065d4767a6d926957cd6fe8661d228d4
Diego Santana <3c.santana@gmail.com>
2024-02-05 22:25:36 -0300

    Ordenando listas de tipos valor

---
+++ AulaInterfaceComparable/AulaInterfaceComparable/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+string path = @"C:\temp\in.txt";
+List<string> names = new List<string>();
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            names.Add(sr.ReadLine());
+        }
+    }
+    // Comparação na ordem natural
+    names.Sort();
+
+    // Lógica parsonalizada
+    // names.Sort((x, y) => x.Length.CompareTo(y.Length));
+
+    // Comparação na ordem decrescente 
+    //names.Sort();
+    //names.Reverse();
+
+    foreach (string name in names)
+    {
+        Console.WriteLine(name);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("Error: ");
+    Console.WriteLine(e.Message);
+}
