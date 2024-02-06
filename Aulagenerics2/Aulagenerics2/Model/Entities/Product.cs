14c5c3e7aedf1fb46bae082e5416e85c7a477171
Diego Santana <3c.santana@gmail.com>
2024-02-06 20:06:52 -0300

    Aula sobre restrições de tipo em generics

---
+++ Aulagenerics2/Aulagenerics2/Model/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aulagenerics2.Model.Entities
+{
+    internal class Product(string name, double price) : IComparable<Product>
+    {
+        public string Name { get; set; } = name;
+        public double Price { get; set; } = price;
+
+        public int CompareTo(Product other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+ 
+ 
