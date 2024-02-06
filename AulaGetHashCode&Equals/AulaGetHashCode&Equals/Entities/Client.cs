e5a9df1a1c1489f31773539f11cd48f534371ae1
Diego Santana <3c.santana@gmail.com>
2024-02-06 20:08:22 -0300

    Aula sobre GetHashCode e Equals

---
+++ AulaGetHashCode&Equals/AulaGetHashCode&Equals/Entities/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace AulaGetHashCode_Equals.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client)) // se obj ñ for uma instancia de Client
+            {
+                return false;    // false = não são iguais 
+            }
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ AulaGetHashCode&Equals/AulaGetHashCode&Equals/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using AulaGetHashCode_Equals.Entities;
+string a = "Maria";
+string b = "Alex";
+string c = "Maria";
+
+Console.WriteLine(a.Equals(b)); // false
+Console.WriteLine(a.Equals(c)); // true
+
+Console.WriteLine();
+
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
+Console.WriteLine(c.GetHashCode());
+
+Console.WriteLine();
+
+Client client1 = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client client2 = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+Console.WriteLine(client1.Equals(client2)); // false
+Console.WriteLine(client1.GetHashCode() == client2.GetHashCode()); // false
