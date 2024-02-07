4bb07b09ecff680f2929a53111d3c2f0502bd108
Diego Santana <3c.santana@gmail.com>
2024-02-07 12:17:20 -0300

    Exerício resolvido sobre conjuntos

---
+++ ExercicioConjuntos/Course/Model/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿namespace Course.Model.Entities
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+        public User(string userName, DateTime instant)
+        {
+            UserName = userName;
+            Instant = instant;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is User))
+            {
+                return false;
+            }
+            User other = obj as User;
+            return this.UserName.Equals(other.UserName);
+        }
+
+        public override string ToString()
+        {
+            return UserName + " " + Instant.ToString();
+        }
+    }
+}
---
+++ ExercicioConjuntos/Course/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Course.Model.Entities;
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+HashSet<User> users = new HashSet<User>();
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string username = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            users.Add(new User(username, instant));
+        }
+        Console.WriteLine("Total users: " +  users.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
