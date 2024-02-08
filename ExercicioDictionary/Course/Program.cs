a4baf3456bfa3622c308f68ccabeb13682191bf3
Diego Santana <3c.santana@gmail.com>
2024-02-07 22:46:49 -0300

    Solução do exercício sobre Dictionary

---
+++ ExercicioDictionary/Course/Program.cs
@@ -0,0 +1,36 @@
+﻿
+try
+{
+    SortedDictionary<string, int> candidates = new SortedDictionary<string, int>();
+    Console.Write("Enter file full path: ");
+    string path = Console.ReadLine();
+
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!(sr.EndOfStream))
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string candidateName = line[0];
+            int votes = int.Parse(line[1]);
+
+            if (candidates.ContainsKey(candidateName))
+            {
+                candidates[candidateName] += votes;
+            }
+            else
+            {
+                candidates[candidateName] = votes;
+            }
+        }
+
+        foreach (var candidate in candidates)
+        {
+            Console.WriteLine(candidate.Key + ": " + candidate.Value);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured");
+    Console.WriteLine(e.Message);
+}
