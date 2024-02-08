9121ebdda26463d214820d075fd95d6a0a4a0a45
Diego Santana <3c.santana@gmail.com>
2024-02-08 19:11:06 -0300

    Aula sobre extensions methods

---
+++ ExtensionMethods/Course/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    internal static class StringExtensions
+    {
+        public static string Cut(this string thisValue, int quantity)
+        {
+            return thisValue.Substring(0, quantity) + "...";
+        }
+    }
+}
---
+++ ExtensionMethods/Course/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime agora = DateTime.Parse("08/02/2024 08:00");
+Console.WriteLine(agora.ElapsedTime()); // 5.1 hours
+
+string s1 = "Good morning dear strudents";
+Console.WriteLine(s1.Cut(10)); // Good morni...
+
+Console.WriteLine(s1.Cut(5)); // Good ...
