c5e8ede8aecf62d6094c056daeef9eda2c35ae19
Diego Santana <3c.santana@gmail.com>
2024-02-07 22:21:39 -0300

    Aula sobre Dictionary, o map do C#

---
+++ AulaDictionary/Course/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Dictionary
+Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies.Add("user", "Maria");
+cookies["email"] = "maria@gmail.com";
+cookies["phone"] = "981814141";
+cookies["phone"] = "971007100";
+
+Console.WriteLine(cookies["user"]);
+Console.WriteLine(cookies["email"]);
+Console.WriteLine(cookies["phone"]);
+
+Console.WriteLine(cookies.Count);
+Console.WriteLine(cookies.ContainsKey("phone"));
+Console.WriteLine(cookies.ContainsValue("Maria"));
+Console.WriteLine();
+cookies.Remove("phone");
+Console.WriteLine(cookies.ContainsKey("phone"));
+
+Console.WriteLine();
+
+foreach(KeyValuePair<string, string> item in cookies )
+{
+    Console.WriteLine(item.Key  + ": " + item.Value);
+}
+
+Console.WriteLine();
+
+foreach(var item in cookies)
+{
+    Console.WriteLine(item.Key + ": " + item.Value);
+}
