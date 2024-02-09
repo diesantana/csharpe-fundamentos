7ff00a2b2f538a3522e4a5b4d2184c8bad37b6f9
Diego Santana <3c.santana@gmail.com>
2024-02-09 00:00:58 -0300

    Introdução aos delegates

---
+++ AulaDelegates/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using Course.Services;
+
+namespace Course
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+
+            BinaryNumericOperation op = CalculateService.Max;
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculateService.Max);
+            double a = 10;
+
+            double b = 12;
+
+            //double result = op.Invoke(a, b);
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AulaDelegates/Course/Services/CalculateService.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Services
+{
+    internal class CalculateService
+    {
+        public static double Max(double x, double y)
+        {
+            return Math.Max(x, y);
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
