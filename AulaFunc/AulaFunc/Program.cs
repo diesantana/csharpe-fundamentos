281e88ee8e63f9cf0f0795c419bbace8252b4d54
Diego Santana <3c.santana@gmail.com>
2024-02-09 19:23:19 -0300

    Aula Func: criando uma função auxiliar e passando ela para o método

---
+++ AulaFunc/AulaFunc/Program.cs
@@ -0,0 +1,27 @@
+﻿using AulaFunc.Entities;
+
+namespace AulaFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("TV", 900.0));
+            products.Add(new Product("Mouse", 50.0));
+            products.Add(new Product("Tablet", 350.50));
+            products.Add(new Product("HD Case", 80.90));
+
+            // chamada do método
+            List<string> result = products.Select(NamesUpper).ToList();
+
+            result.ForEach(x => Console.WriteLine(x));
+        }
+
+        // Método auxiliar que corresponde a assinatura do Func
+        public static string NamesUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
