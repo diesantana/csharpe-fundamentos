acab61fa613dd6d014d5fbe6c4f3264f9b30ab33
Diego Santana <3c.santana@gmail.com>
2024-02-10 16:18:50 -0300

    feat: aula de demonstração das operações com o LINQ

--- AulaLinq2/Course/Program.cs
+++ AulaLinq2/Course/Program.cs
@@ -4,7 +4,7 @@ namespace Course
 {
     internal class Program
     {
-        static void PrintList<T>(string message, IEnumerable<T> collection) 
+        static void PrintList<T>(string message, IEnumerable<T> collection)
         {
             Console.WriteLine(message + ":");
             foreach (var item in collection)
@@ -34,7 +34,56 @@ namespace Course
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            PrintList("Produtos", products);
+            // TIER 1 AND PRICE < 900
+            var teste1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            PrintList("Tier 1 and Price < 900", teste1);
+
+            // NAMES OF PRODUCTS FROM TOOLS
+            var teste2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            PrintList("Names of product from tools", teste2);
+
+            // NAMES STARTED WITH 'C'
+            var teste3 = products.Where(p => p.Name[0] == 'c' || p.Name[0] == 'C');
+            PrintList("Names started with C", teste3);
+
+            // NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT
+            var teste4 = products
+                .Where(p => p.Name[0] == 'c' || p.Name[0] == 'C')
+                .Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            PrintList("Names started with C and anonymous object", teste4);
+
+            // PRODUCTS WITH TIER 1, ORDER PER PRICE
+            var teste5 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price);
+            PrintList("Products with tier 1, ORDER PER PRICE", teste5);
+            // ORDER PER PRICE AND PER NAME
+            var teste6 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            PrintList("Products with tier 1, ORDER PER PRICE AND NAME", teste6);
+
+            // USE TEST 6: IGNORE THE 2 FIRST AND TAKE 4
+            var teste7 = teste6.Skip(2).Take(4);
+            PrintList("SKIP 2 THEN TAKE 4", teste7);
+
+            // FIRST ELEMENT
+            var teste8 = products.FirstOrDefault();
+            Console.WriteLine("FIRST: " + teste8);
+            Console.WriteLine();
+
+            // FIRST ELEMENT OR NULL
+            var teste9 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            Console.WriteLine("FIRST NULL: " + teste9);
+            Console.WriteLine();
+
+            // SINGLE VALUE
+            var teste10 = products.Where(p => p.Id == 3).SingleOrDefault();
+            Console.WriteLine("SINGLE VALUE: " + teste10);
+            Console.WriteLine();
+
+            // SINGLE VALUE NULL
+            var teste11 = products.Where(p => p.Id == 30).SingleOrDefault();
+            Console.WriteLine("SINGLE VALUE NULL: " + teste11);
+            Console.WriteLine();
+
         }
+
     }
 }
