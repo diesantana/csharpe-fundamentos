ecfc9dd1789d03a7a3dbe1101255a39d2e838b60
Diego Santana <3c.santana@gmail.com>
2024-02-10 11:16:29 -0300

    Initial commit

---
+++ AulaLinq2/Course/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Course.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Id);
+            sb.Append(", ");
+            sb.Append(Name);
+            sb.Append(", ");
+            sb.Append(Price.ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(", ");
+            sb.Append(Category.Name);
+            sb.Append(", ");
+            sb.Append(Category.Tier);
+            return sb.ToString();
+        }
+
+
+    }
+}
