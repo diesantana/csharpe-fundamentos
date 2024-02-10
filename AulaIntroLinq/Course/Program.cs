e303d611e758d8aa454c7c2e8c9b86fe862c9c76
Diego Santana <3c.santana@gmail.com>
2024-02-10 00:39:21 -0300

    Aula de introdução ao LINQ

---
+++ AulaIntroLinq/Course/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Specify the data source
+            int[] numbers = { 2, 3, 4, 5};
+
+            // Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(num => num % 2 == 0)
+                .Select(num => num * 10);
+            
+            // Execute the query
+            foreach (int num in result)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
